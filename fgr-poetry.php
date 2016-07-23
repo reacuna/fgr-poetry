2f4565770c1f08cb21698a6f3ec94d4d30baea45
Raúl Acuña <reacuna@crac.cl>
2016-07-22 23:33:21 -0400

    Update text domain

--- fgr-poetry.php
+++ fgr-poetry.php
@@ -14,19 +14,19 @@
 function poem_init() {
 	register_post_type( 'poem', array(
 		'labels'            => array(
-			'name'                => __( 'Poems', 'YOUR-TEXTDOMAIN' ),
-			'singular_name'       => __( 'Poem', 'YOUR-TEXTDOMAIN' ),
-			'all_items'           => __( 'All Poems', 'YOUR-TEXTDOMAIN' ),
-			'new_item'            => __( 'New poem', 'YOUR-TEXTDOMAIN' ),
-			'add_new'             => __( 'Add New', 'YOUR-TEXTDOMAIN' ),
-			'add_new_item'        => __( 'Add New poem', 'YOUR-TEXTDOMAIN' ),
-			'edit_item'           => __( 'Edit poem', 'YOUR-TEXTDOMAIN' ),
-			'view_item'           => __( 'View poem', 'YOUR-TEXTDOMAIN' ),
-			'search_items'        => __( 'Search poems', 'YOUR-TEXTDOMAIN' ),
-			'not_found'           => __( 'No poems found', 'YOUR-TEXTDOMAIN' ),
-			'not_found_in_trash'  => __( 'No poems found in trash', 'YOUR-TEXTDOMAIN' ),
-			'parent_item_colon'   => __( 'Parent poem', 'YOUR-TEXTDOMAIN' ),
-			'menu_name'           => __( 'Poems', 'YOUR-TEXTDOMAIN' ),
+			'name'                => __( 'Poems', 'fgr-poetry' ),
+			'singular_name'       => __( 'Poem', 'fgr-poetry' ),
+			'all_items'           => __( 'All Poems', 'fgr-poetry' ),
+			'new_item'            => __( 'New poem', 'fgr-poetry' ),
+			'add_new'             => __( 'Add New', 'fgr-poetry' ),
+			'add_new_item'        => __( 'Add New poem', 'fgr-poetry' ),
+			'edit_item'           => __( 'Edit poem', 'fgr-poetry' ),
+			'view_item'           => __( 'View poem', 'fgr-poetry' ),
+			'search_items'        => __( 'Search poems', 'fgr-poetry' ),
+			'not_found'           => __( 'No poems found', 'fgr-poetry' ),
+			'not_found_in_trash'  => __( 'No poems found in trash', 'fgr-poetry' ),
+			'parent_item_colon'   => __( 'Parent poem', 'fgr-poetry' ),
+			'menu_name'           => __( 'Poems', 'fgr-poetry' ),
 		),
 		'public'            => true,
 		'hierarchical'      => true,
@@ -53,19 +53,19 @@ function poem_updated_messages( $messages ) {
 
 	$messages['poem'] = array(
 		0 => '', // Unused. Messages start at index 1.
-		1 => sprintf( __('Poem updated. <a target="_blank" href="%s">View poem</a>', 'YOUR-TEXTDOMAIN'), esc_url( $permalink ) ),
-		2 => __('Custom field updated.', 'YOUR-TEXTDOMAIN'),
-		3 => __('Custom field deleted.', 'YOUR-TEXTDOMAIN'),
-		4 => __('Poem updated.', 'YOUR-TEXTDOMAIN'),
+		1 => sprintf( __('Poem updated. <a target="_blank" href="%s">View poem</a>', 'fgr-poetry'), esc_url( $permalink ) ),
+		2 => __('Custom field updated.', 'fgr-poetry'),
+		3 => __('Custom field deleted.', 'fgr-poetry'),
+		4 => __('Poem updated.', 'fgr-poetry'),
 		/* translators: %s: date and time of the revision */
-		5 => isset($_GET['revision']) ? sprintf( __('Poem restored to revision from %s', 'YOUR-TEXTDOMAIN'), wp_post_revision_title( (int) $_GET['revision'], false ) ) : false,
-		6 => sprintf( __('Poem published. <a href="%s">View poem</a>', 'YOUR-TEXTDOMAIN'), esc_url( $permalink ) ),
-		7 => __('Poem saved.', 'YOUR-TEXTDOMAIN'),
-		8 => sprintf( __('Poem submitted. <a target="_blank" href="%s">Preview poem</a>', 'YOUR-TEXTDOMAIN'), esc_url( add_query_arg( 'preview', 'true', $permalink ) ) ),
-		9 => sprintf( __('Poem scheduled for: <strong>%1$s</strong>. <a target="_blank" href="%2$s">Preview poem</a>', 'YOUR-TEXTDOMAIN'),
+		5 => isset($_GET['revision']) ? sprintf( __('Poem restored to revision from %s', 'fgr-poetry'), wp_post_revision_title( (int) $_GET['revision'], false ) ) : false,
+		6 => sprintf( __('Poem published. <a href="%s">View poem</a>', 'fgr-poetry'), esc_url( $permalink ) ),
+		7 => __('Poem saved.', 'fgr-poetry'),
+		8 => sprintf( __('Poem submitted. <a target="_blank" href="%s">Preview poem</a>', 'fgr-poetry'), esc_url( add_query_arg( 'preview', 'true', $permalink ) ) ),
+		9 => sprintf( __('Poem scheduled for: <strong>%1$s</strong>. <a target="_blank" href="%2$s">Preview poem</a>', 'fgr-poetry'),
 		// translators: Publish box date format, see http://php.net/date
 		date_i18n( __( 'M j, Y @ G:i' ), strtotime( $post->post_date ) ), esc_url( $permalink ) ),
-		10 => sprintf( __('Poem draft updated. <a target="_blank" href="%s">Preview poem</a>', 'YOUR-TEXTDOMAIN'), esc_url( add_query_arg( 'preview', 'true', $permalink ) ) ),
+		10 => sprintf( __('Poem draft updated. <a target="_blank" href="%s">Preview poem</a>', 'fgr-poetry'), esc_url( add_query_arg( 'preview', 'true', $permalink ) ) ),
 	);
 
 	return $messages;
